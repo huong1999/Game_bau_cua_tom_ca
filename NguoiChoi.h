e2d404f40e564f88f11bbc3b2c65e4368f17e95e
huong1999 <37472308+huong1999@users.noreply.github.com>
2018-05-10 16:42:46 +0700

    Update NguoiChoi.h

--- NguoiChoi.h
+++ NguoiChoi.h
@@ -1,7 +1,13 @@
 #ifndef NGUOICHOI_H_INCLUDED
 #define NGUOICHOI_H_INCLUDED
 #include<iostream>
+#include<conio.h>
 #include<cstdlib>
+
+#include"HienThi.h"
+
+using namespace std;
+
 
 char luaChonChoi();     //lua chon choi tiep hoac thoat game
 
