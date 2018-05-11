02344d76c6f47e9142b12028d7df9e1d50938128
huong1999 <37472308+huong1999@users.noreply.github.com>
2018-05-11 10:55:37 +0700

    Update HienThi.cpp

--- HienThi.cpp
+++ HienThi.cpp
@@ -4,7 +4,7 @@
 
 void inMenu(){
 	TextColor (10);
-	cout<<" ___________________________________________________\n<___________________GAME BAU CUA____________________>\n\n";			//cau hinh menu
+	cout<<" ___________________________________________________\n<_________________GAME BAU CUA TOM CA_______________>\n\n";			//cau hinh menu
 	TextColor (14);
 	cout<<"            O"<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<"> MENU <"<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<"O\n                "<<char(176)<<"                    "<<char(176)<<"\n                "<<char(176)<<"  1-BAT DAU CHOI    "<<char(176)<<"\n                "<<char(176)<<"  2-HUONG DAN CHOI  "<<char(176)<<"\n                "<<char(176)<<"  3-THOAT           "<<char(176)<<"\n                "<<char(176)<<"                    "<<char(176)<<"\n            O"<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<"O\n";
 	TextColor (10);
@@ -18,7 +18,7 @@ void inHuongDan(){
 	cout<<"HUONG DAN CHOI TRO CHOI BAU CUA TOM CA";
 	TextColor (11);
 	cout<<"__________________";
-	cout<<"\n\\  /                                                                     \\  /\n "<<char(176)<<char(176)<<"    Voi cach choi tuong tu game xoc dia, nguoi choi chon 1 trong 6     "<<char(176)<<char(176)<<"\n "<<char(176)<<char(176)<<"    linh vat may man là bau,cua,tom, ca,ga,nai,sau do lan luot dat     "<<char(176)<<char(176)<<"\n "<<char(176)<<char(176)<<"    cuoc voi nhieu muc cuoc khac nhau. Linh vat nao co mot trong 1     "<<char(176)<<char(176)<<"\n "<<char(176)<<char(176)<<"    trong 3 ket qua ma nha cai dua ra thi nguoi choi se duoc cong      "<<char(176)<<char(176)<<"\n "<<char(176)<<char(176)<<"    tien cuoc,nguoc lai ban se bi tru so tien da cuoc vao linh vat     "<<char(176)<<char(176)<<"\n "<<char(176)<<char(176)<<"     do.Ban cung se duoc nhan 10coin khi bat dau tham gia tro choi     "<<char(176)<<char(176)<<"\n/__\\_____________________________________________________________________/__\\\n\n\n";
+	cout<<"\n\\  /                                                                     \\  /\n "<<char(176)<<char(176)<<"    Voi cach choi tuong tu game xoc dia, nguoi choi chon 1 trong 6     "<<char(176)<<char(176)<<"\n "<<char(176)<<char(176)<<"    linh vat may man la bau,cua,tom, ca,ga,nai,sau do lan luot dat     "<<char(176)<<char(176)<<"\n "<<char(176)<<char(176)<<"    cuoc voi nhieu muc cuoc khac nhau. Linh vat nao co mot trong 1     "<<char(176)<<char(176)<<"\n "<<char(176)<<char(176)<<"    trong 3 ket qua ma nha cai dua ra thi nguoi choi se duoc cong      "<<char(176)<<char(176)<<"\n "<<char(176)<<char(176)<<"    tien cuoc,nguoc lai ban se bi tru so tien da cuoc vao linh vat     "<<char(176)<<char(176)<<"\n "<<char(176)<<char(176)<<"     do.Ban cung se duoc nhan 10coin khi bat dau tham gia tro choi     "<<char(176)<<char(176)<<"\n/__\\_____________________________________________________________________/__\\\n\n\n";
 	TextColor (12);
 	cout<<"Nhan phim bat ki de quay lai menu";
 }
@@ -31,8 +31,8 @@ void chuyenTenBien(int linhVat){
 	if(linhVat==5)	cout<<"\n + So tien ban dat cuoc vao nai la: ";
 	if(linhVat==6)	cout<<"\n + So tien ban dat cuoc vao ga la: ";
 }
-void chuyenTenketQua(int con1,int con2,int con3){
-	cout<<"\n____________________________\n";
+void chuyenTenketQua(int &con1,int &con2,int &con3){
+	cout<<"\n_____________________________________\n";
 		TextColor (12);
 	cout<<"Ket qua:";
 
